6020cb33575e2801078669b515c860a21c253c7e
Ilya Emelyanov <emel.9616@gmail.com>
2021-08-22 17:52:20 +0700

    Test

---
+++ install_package.py
@@ -0,0 +1,29 @@
+import sys
+import subprocess
+from jsonschema.exceptions import best_match
+import pkg_resources
+
+
+class Package:
+
+    def __init__(self, required_package):
+        self.required_package = required_package
+
+    def install(self):
+        install_packages = {package.key for package in pkg_resources.working_set}
+        missing_packages = []
+        for package in self.required_package:
+            package_is_missing = True
+            for install_package in install_packages:
+                if package == install_package:
+                   package_is_missing = False
+                   break
+            
+            if package_is_missing is True:
+                missing_packages.append(package)
+
+        if missing_packages:
+            try:
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing_packages])
+            except Exception as e:
+                raise Exception(f'Failed to install package - {missing_packages}. Exception [{str(e)}]')
--- start_integration.py
+++ start_integration.py
@@ -1,42 +1,50 @@
+from install_package import Package
+import json
+
+
+with open('settings.json', "rb") as PFile:
+    setting_data = json.loads(PFile.read().decode('utf-8'))
+
+required_package = setting_data["packages"]
+Package(required_package).install()
+
+
 from jsonschema import validate
 from integration_log import build_logger
 from upsource_integration import Issue, IssueTask, Review, Integration, IntegrationError
-import json
 import re
 
-with open('settings.json', "rb") as PFile:
-    password_data = json.loads(PFile.read().decode('utf-8'))
 
 with open('settings_schema.json', "rb") as PFile:
     data_schema = json.loads(PFile.read().decode('utf-8'))
 
 try:
-    validate(instance=password_data, schema=data_schema)
+    validate(instance=setting_data, schema=data_schema)
 except Exception as e:
     raise IntegrationError(f'Incorrect value in the settings file\n{str(e)}')
 
-url_upsource = password_data["urlUpsource"]
-user_name_upsource = password_data["userNameUpsource"]
-token_upsource = password_data["tokenUpsource"]
-products = password_data["products"]
-reviewers = password_data["reviewers"]
+url_upsource = setting_data["urlUpsource"]
+user_name_upsource = setting_data["userNameUpsource"]
+token_upsource = setting_data["tokenUpsource"]
+products = setting_data["products"]
+reviewers = setting_data["reviewers"]
 
-if re.search('https', password_data["urlOneVizion"]) is None:
-    url_onevizion_without_protocol = re.sub("^http://", "", password_data["urlOneVizion"][:-1])
+if re.search('https', setting_data["urlOneVizion"]) is None:
+    url_onevizion_without_protocol = re.sub("^http://", "", setting_data["urlOneVizion"][:-1])
 else:
-    url_onevizion_without_protocol = re.sub("^https://", "", password_data["urlOneVizion"][:-1])
+    url_onevizion_without_protocol = re.sub("^https://", "", setting_data["urlOneVizion"][:-1])
 
-url_onevizion = password_data["urlOneVizion"]
-login_onevizion = password_data["loginOneVizion"]
-pass_onevizion = password_data["passOneVizion"]
-issue_trackor_type = password_data["issueTrackorType"]
-issue_task_trackor_type = password_data["issueTaskTrackorType"]
+url_onevizion = setting_data["urlOneVizion"]
+login_onevizion = setting_data["loginOneVizion"]
+pass_onevizion = setting_data["passOneVizion"]
+issue_trackor_type = setting_data["issueTrackorType"]
+issue_task_trackor_type = setting_data["issueTaskTrackorType"]
 
-issue_statuses = password_data["issueStatuses"]
-issue_fields = password_data["issueFields"]
-issue_task_fields = password_data["issueTaskFields"]
-issue_task_types = password_data["issueTaskTypes"]
-issue_task_statuses = password_data["issueTaskStatuses"]
+issue_statuses = setting_data["issueStatuses"]
+issue_fields = setting_data["issueFields"]
+issue_task_fields = setting_data["issueTaskFields"]
+issue_task_types = setting_data["issueTaskTypes"]
+issue_task_statuses = setting_data["issueTaskStatuses"]
 
 logger = build_logger()
 issue = Issue(url_onevizion_without_protocol, login_onevizion, pass_onevizion,
